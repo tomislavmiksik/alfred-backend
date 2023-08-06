d8c84e027d20f0d584c26b1691dcafc3cbeb0b57
Tomislav Mikšik <tomislav.miksik@gmail.com>
2023-08-07 00:23:54 +0200

    fix: S3 setup

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,6 +1,34 @@
 export default [
   'strapi::errors',
-  'strapi::security',
+  /* Replace 'strapi::security', with this snippet */
+  /* Beginning of snippet */
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'alfred-strapi-aws-s3-images-bucket.s3.eu-central-1.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'alfred-strapi-aws-s3-images-bucket.s3.eu-central-1.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  /* End of snippet */
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
---
+++ src/api/task/controllers/task.ts
@@ -0,0 +1,66 @@
+/**
+ * task controller
+ */
+
+import {factories} from '@strapi/strapi'
+
+export default factories.createCoreController('api::task.task', ({ strapi }) => {
+  return {
+    async find(ctx, next) {
+      const user = ctx.state.user
+      strapi.log.debug(JSON.stringify(ctx.state));
+      if (!user) {
+        return ctx.badRequest(null, [{ messages: [{ id: "No auth header found" }] }])
+      }
+
+      const data = await strapi.entityService.findMany("api::task.task", {
+        filters: {
+          "author": {
+            "id": user.id
+          }
+        },
+
+      });
+      if (!data) {
+        return ctx.notFound();
+      }
+
+      const sanitizedEvents = await this.sanitizeOutput(data, ctx);
+
+      return this.transformResponse(sanitizedEvents);
+    },
+    async create(ctx, next) {
+      const {id} = ctx.state.user; //ctx.state.user contains the current authenticated user
+
+      console.log('ctx.request.body', ctx.request.body);
+
+      const response = await super.create(ctx);
+      return await strapi.entityService
+        .update('api::task.task', response.data.id, {data: {author: id}});
+    },
+
+    async update(ctx, next) {
+      const {id} = ctx.state.user;
+      const {id: taskId} = ctx.params;
+      const response = await super.update(ctx);
+      return await strapi.entityService
+        .update('api::task.task', taskId, {data: {author: id}});
+    },
+
+    async delete(ctx, next) {
+      const {id} = ctx.state.user;
+      const {id: taskId} = ctx.params;
+      const response = await super.delete(ctx);
+      return await strapi.entityService
+        .delete('api::task.task', taskId, {data: {author: id}});
+    },
+
+    async findOne(ctx, next) {
+      const {id} = ctx.state.user;
+      const {id: taskId} = ctx.params;
+      const response = await super.findOne(ctx);
+      return await strapi.entityService
+        .findOne('api::task.task', taskId, {data: {author: id}});
+    }
+  }
+});
